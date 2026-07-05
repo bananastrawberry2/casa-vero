6e4e260424596532069bdb324cf0e6f2e45b1924
bananastrawberry2 <sifukatana07@gmail.com>
2026-07-05 22:23:35 +0300

    fix Stripe API version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -10,7 +10,7 @@ export function getStripeServer(): Stripe {
     throw new Error("STRIPE_SECRET_KEY is not set");
   }
   return new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2024-11-20.acacia",
+    apiVersion: "2025-02-24.acacia",
   });
 }
 
